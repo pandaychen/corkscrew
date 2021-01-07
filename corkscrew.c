7cca130cefcfb41552d179b9b20150f615ac4579
pandaychen <panda.chenyong@gmail.com>
2021-01-07 09:15:23 +0000

    修改corkscrew，支持多级CONNECT代理连接

--- corkscrew.c
+++ corkscrew.c
@@ -1,4 +1,3 @@
-#include "config.h"
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -11,6 +10,7 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <assert.h>
 
 #if HAVE_SYS_FILIO_H
 #include <sys/filio.h>
@@ -31,6 +31,18 @@ int sock_connect PARAMS((const char *hname, int port));
 int main PARAMS((int argc, char *argv[]));
 
 #define BUFSIZE 4096
+#define PROXY_IP_LEN 128
+#define PROXY_PORT_LEN 6
+#define VERSION "pv1"
+
+//add proxy struct
+struct __proxy{
+	char szProxyip[PROXY_IP_LEN];
+	char szProxyport[PROXY_PORT_LEN];
+};
+
+typedef struct __proxy Proxy;
+
 /*
 char linefeed[] = "\x0A\x0D\x0A\x0D";
 */
@@ -128,7 +140,7 @@ void usage ()
 #endif
 {
 	printf("corkscrew %s (agroman@agroman.net)\n\n", VERSION);
-	printf("usage: corkscrew <proxyhost> <proxyport> <desthost> <destport> [authfile]\n");
+	printf("usage: corkscrew <proxyhost1> <proxyport1> <proxyhost2> <proxyport2>... <desthost> <destport> [authfile]\n");
 }
 
 #ifdef ANSI_FUNC
@@ -182,40 +194,42 @@ char *argv[];
 	ssize_t len;
 	FILE *fp;
 
+	int proxy_num = 0;
+	int succ_proxy_count = 0;
+
 	port = 80;
 
-	if (argc == 5 || argc == 6) {
-		if (argc == 5) {
-			host = argv[1];
-			port = atoi(argv[2]);
-			desthost = argv[3];
-			destport = argv[4];
-			up = getenv("CORKSCREW_AUTH");
-		}
-		if (argc == 6) {
-			host = argv[1];
-			port = atoi(argv[2]);
-			desthost = argv[3];
-			destport = argv[4];
-			fp = fopen(argv[5], "r");
-			if (fp == NULL) {
-				fprintf(stderr, "Error opening %s: %s\n", argv[5], strerror(errno));
-				exit(-1);
-			} else {
-				if (!fscanf(fp, "%4095s", line)) {
-					fprintf(stderr, "Error reading auth file's content\n");
-					exit(-1);
-				}
+	Proxy *psshproxy = NULL;
 
-				up = line;
-				fclose(fp);
-			}
-		}
-	} else {
+	//ssh -o "ProxyCommand corkscrew proxyhostname(代理IP) proxyport(代理port) %h %p" loginname@ip
+	if (argc < 3){
 		usage();
 		exit(-1);
 	}
 
+	proxy_num = (argc-3)/2;
+
+	//cancel proxy auth
+	psshproxy = (Proxy *)malloc(proxy_num*sizeof(*psshproxy));
+	assert(psshproxy != NULL);
+	int i = 0;
+	for (; i < proxy_num; i++){
+		snprintf(psshproxy[i].szProxyip, sizeof(psshproxy[i].szProxyip), "%s", argv[1 + 2*i]);
+		snprintf(psshproxy[i].szProxyport, sizeof(psshproxy[i].szProxyport), "%s", argv[1 + 2 * i + 1]);
+	}
+
+
+	for (i = 0; i < proxy_num; i++){
+		printf("%s,%d\n", psshproxy[i].szProxyip, psshproxy[i].szProxyport);
+	}
+
+	desthost = argv[1+2*i];			//要连接的目的IP
+	destport = argv[1+2*i+1];		//要连接的目的PORT
+
+	/*	拼接CONNECT URL
+		url:CONNECT 1.2.3.4:36000 HTTP/1.0
+		response:HTTP / 1.1 200 Connection established
+	*/
 	strncpy(uri, "CONNECT ", sizeof(uri));
 	strncat(uri, desthost, sizeof(uri) - strlen(uri) - 1);
 	strncat(uri, ":", sizeof(uri) - strlen(uri) - 1);
@@ -227,18 +241,51 @@ char *argv[];
 	}
 	strncat(uri, linefeed, sizeof(uri) - strlen(uri) - 1);
 
-	csock = sock_connect(host, port);
+	//CONNECT THE FIRST SERVER
+	csock = sock_connect(psshproxy[0].szProxyip, atoi(psshproxy[0].szProxyport));
 	if(csock == -1) {
 		fprintf(stderr, "Couldn't establish connection to proxy: %s\n", strerror(errno));
 		exit(-1);
 	}
 
-	sent = 0;
-	setup = 0;
-	for(;;) {
+	//proxy connect
+	sent = 0;	
+	setup = 0;	
+	succ_proxy_count++;
+	for (;;){
 		FD_ZERO(&sfd);
 		FD_ZERO(&rfd);
-		if ((setup == 0) && (sent == 0)) {
+
+		if (setup == 1 && sent == 1){
+			succ_proxy_count++;
+			setup = 0;
+			sent = 0;
+		}
+
+		if (succ_proxy_count > proxy_num){
+			break;
+		}
+
+		memset(uri, 0, BUFSIZE);
+		if (succ_proxy_count == proxy_num){
+			//connect to real server
+			strncpy(uri, "CONNECT ", sizeof(uri));
+			strncat(uri, desthost, sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, ":", sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, destport, sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, " HTTP/1.0", sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, linefeed, sizeof(uri)-strlen(uri) - 1);
+		}
+		else{
+			strncpy(uri, "CONNECT ", sizeof(uri));
+			strncat(uri, psshproxy[succ_proxy_count].szProxyip, sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, ":", sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, psshproxy[succ_proxy_count].szProxyport, sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, " HTTP/1.0", sizeof(uri)-strlen(uri) - 1);
+			strncat(uri, linefeed, sizeof(uri)-strlen(uri) - 1);
+		}
+
+		if ((setup == 0) && (sent == 0) ) {
 			FD_SET(csock, &sfd);
 		}
 		FD_SET(csock, &rfd);
@@ -246,50 +293,68 @@ char *argv[];
 
 		tv.tv_sec = 5;
 		tv.tv_usec = 0;
-
-		if(select(csock+1,&rfd,&sfd,NULL,&tv) == -1) break;
+		if (select(csock + 1, &rfd, &sfd, NULL, &tv) == -1) break;
 
 		/* there's probably a better way to do this */
 		if (setup == 0) {
+			//接收数据
 			if (FD_ISSET(csock, &rfd)) {
 				len = read(csock, buffer, sizeof(buffer));
-				if (len<=0)
+				if (len <= 0)
 					break;
 				else {
-					memset(descr, 0, sizeof(descr));
-					sscanf(buffer,"%s%d%[^\n]",version,&code,descr);
-					if ((strncmp(version,"HTTP/",5) == 0) && (code >= 200) && (code < 300))
+					sscanf(buffer, "%s%d%[^\n]", version, &code, descr);
+					if ((strncmp(version, "HTTP/", 5) == 0) && (code >= 200) && (code < 300)){
 						setup = 1;
+					}
 					else {
-						if ((strncmp(version,"HTTP/",5) == 0) && (code >= 407)) {
+						if ((strncmp(version, "HTTP/", 5) == 0) && (code >= 407)) {
 						}
-						fprintf(stderr, "Proxy could not open connection to %s: %s\n", desthost, descr);
+						fprintf(stderr, "Proxy could not open connnection to %s: %s\n", desthost, descr);
 						exit(-1);
 					}
 				}
 			}
 			if (FD_ISSET(csock, &sfd) && (sent == 0)) {
 				len = write(csock, uri, strlen(uri));
-				if (len<=0)
+				if (len <= 0)
 					break;
 				else
 					sent = 1;
 			}
-		} else {
-			if (FD_ISSET(csock, &rfd)) {
-				len = read(csock, buffer, sizeof(buffer));
-				if (len<=0) break;
-				len = write(1, buffer, len);
-				if (len<=0) break;
-			}
-
-			if (FD_ISSET(0, &rfd)) {
-				len = read(0, buffer, sizeof(buffer));
-				if (len<=0) break;
-				len = write(csock, buffer, len);
-				if (len<=0) break;
-			}
 		}
 	}
+
+	//proxy connect done
+	for(;;) {
+		FD_ZERO(&sfd);
+		FD_ZERO(&rfd);
+		FD_SET(csock, &rfd);
+		FD_SET(0, &rfd);
+
+		tv.tv_sec = 5;
+		tv.tv_usec = 0;
+			
+		if(select(csock+1,&rfd,&sfd,NULL,&tv) == -1) break;
+
+	
+		//forwarding dataflow
+		if (FD_ISSET(csock, &rfd)) {
+			len = read(csock, buffer, sizeof(buffer));
+			if (len<=0) break;
+			len = write(1, buffer, len);
+			if (len<=0) break;
+		}
+
+		if (FD_ISSET(0, &rfd)) {
+			//0
+			len = read(0, buffer, sizeof(buffer));
+			if (len<=0) break;
+			len = write(csock, buffer, len);
+			if (len<=0) break;
+		}	
+	}
+
+	if (psshproxy)	free(psshproxy);
 	exit(0);
 }
